c1ed27949e517ad257cd4c8de088991db78a761c
marwan-tabib <marwanmaroc297@gmail.com>
2023-12-15 11:02:48 +0100

    dev | Make src\main\java\com\example\brief_gestion_competitions_aftas\repository\LevelRepository.java file, with JpaRepository extends

---
+++ src/main/java/com/example/brief_gestion_competitions_aftas/repository/LevelRepository.java
@@ -0,0 +1,7 @@
+package com.example.brief_gestion_competitions_aftas.repository;
+
+import com.example.brief_gestion_competitions_aftas.model.Level;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LevelRepository extends JpaRepository<Level, Long> {
+}
