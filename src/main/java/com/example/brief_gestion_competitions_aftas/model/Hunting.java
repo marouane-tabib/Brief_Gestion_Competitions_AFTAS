83d25210de2179dd9016cabaed866ab000513b0c
marwan-tabib <marwanmaroc297@gmail.com>
2023-12-15 10:58:23 +0100

    dev | Make src\main\java\com\example\brief_gestion_competitions_aftas\model\Hunting.java file, with validatin|relationships|entity columns

---
+++ src/main/java/com/example/brief_gestion_competitions_aftas/model/Hunting.java
@@ -0,0 +1,46 @@
+package com.example.brief_gestion_competitions_aftas.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Hunting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int numberOfFish;
+
+    @ManyToOne
+    private Competition competition;
+
+    @ManyToOne
+    private Member member;
+
+    @ManyToOne
+    private Fish fish;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date updatedAt;
+}
